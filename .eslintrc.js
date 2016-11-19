fc2b01bb3be2f16ad3ece06d17f4ca1ff35c1db1
Dave Moloney <whatsyourfavouritehummingnoise@gmail.com>
2016-11-19 10:37:09 +0000

    ESlint changes: Adding personal preference adjustments to ESLint and justification for same to README

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,15 @@ module.exports = {
     "extends": "google",
     "parserOptions": {
         "ecmaVersion": 6
+    },
+    "rules": {
+        "arrow-parens": ["error", "as-needed"],
+        "no-multi-spaces": [
+            "error",
+            {
+                "exceptions": { "VariableDeclarator": true }
+            }
+        ],
+	"require-jsdoc": 0
     }
 };
